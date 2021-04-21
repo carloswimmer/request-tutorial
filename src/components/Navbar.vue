ce5e4d86ddb42dd96bb9f3941e04b0e0c634a596
Carlos Wimmer <coiote777@gmail.com>
2021-04-20 23:29:38 -0300

    Create Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,27 @@
 <template>
   <nav>
-    <v-app-bar app absolute color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar app dark color="primary">
+      <v-btn icon>
+        <v-icon large>mdi-nfc-tap</v-icon>
+      </v-btn>
 
-      <v-toolbar-title>Title</v-toolbar-title>
+      <v-toolbar-title
+        >REQUEST
+        <strong> TUTORIAL </strong>
+      </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
+      <v-btn to="/" icon>
+        <v-icon>mdi-account-multiple</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
+      <v-btn to="posts" icon>
+        <v-icon>mdi-note-text-outline</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
+      <v-btn to="albums" icon>
+        <v-icon>mdi-image-album</v-icon>
       </v-btn>
     </v-app-bar>
   </nav>
@@ -25,5 +30,7 @@
 <script>
 export default {
   name: "Navbar",
+
+  methods: {},
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import About from "../views/About.vue";
+import Users from "../views/Users.vue";
+import Posts from "../views/Posts.vue";
+import Albums from "../views/Albums.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Users",
+    component: Users,
   },
   {
-    path: "/about",
-    name: "About",
-    component: About,
+    path: "/posts",
+    name: "Posts",
+    component: Posts,
+  },
+  {
+    path: "/albums",
+    name: "Albums",
+    component: Albums,
   },
 ];
 
